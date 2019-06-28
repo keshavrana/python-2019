9e41caaf844df5a18a97bf4fff84c2dcf4a475f2
beena beena <keshavrana>
2019-06-28 04:21:20 -0700

    python basics

---
+++ Assignment 1.py
@@ -0,0 +1,49 @@
+1. print statment
+
+print("Hello Welcome To My New Application In This Application I Teach You Some Specific And ")
+print('keshav' "rana  /" *10)
+2. addition two number
+
+a = 10;
+print(a)
+b =5;
+print(b)
+riya=int(a+b);
+print(riya)
+3. take input from user
+
+name=input("whats your name pleases enter => ")
+color =input("your favorite color => ")
+print( name + " likes " + color )
+
+4. age calculation
+brith_date=input("Enter your DOB =>")
+age=2019-int(brith_date);
+print("your age is => "+ str(age))
+
+5.wiegth calculation
+keshav=input('enter your name =>')
+print('welcome mr. '+keshav)
+weight=input("enter your weight in pounds => ")
+kilogram=int(weight)*0.45
+print(kilogram)
+
+6. varibles in python
+name="youtube"
+print(name[6])
+
+7. data type in java
+num=complex(5)
+print(type(num))
+print(num)
+print(range(10))
+a=input("enter the valuse of your number => ")
+b=input("enter the seocnd number => ")
+c=int(a)/int(b)
+print(bool(c))
+
+8. dictionary in python
+d={'rana':'ipsa',32:'jyoti',33:'kamal',34:'karan',35:'keshav'}
+print(d)
+d[35]='keshav'
+print
