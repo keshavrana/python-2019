b43b4f23af60bfe4877f4515d9fcc903ffbe9cef
beena beena <keshavrana>
2019-06-28 03:36:02 -0700

    django

---
+++ test.py
@@ -0,0 +1,46 @@
+#print("Hello Welcome To My New Application In This Application I Teach You Some Specific And ")
+#print('keshav' "rana  /" *10)
+#a = 10;
+#print(a)
+#b =5;
+#print(b)
+#riya=a+b;
+#print(riya)
+
+#name=input("whats your name pleases enter => ")
+#color =input("your favorite color => ")
+#print( name + " likes " + color )
+
+#brith_date=input("Enter your DOB =>")
+#age=2019-int(brith_date);
+#print("your age is => "+ str(age))
+#keshav=input('enter your name =>')
+#print('welcome mr. '+keshav)
+#weight=input("enter your weight in pounds => ")
+#kilogram=int(weight)*0.45
+#print(kilogram)
+#a=input("enter a number   ")
+#b=input("enter b number   ")
+#riya=a+b
+#print(riya)
+
+
+#keriya=input("enter your user name plz => ")
+#print("welcome back "+riya)
+#name=input("enter your age => ")
+#print("your age is " +name)
+
+#name="youtube"
+#print(name[6])
+#num=complex(5)
+#print(type(num))
+#print(num)
+#print(range(10))
+#a=input("enter the valuse of your number => ")
+#b=input("enter the seocnd number => ")
+#c=int(a)/int(b)
+#print(bool(c))
+d={'rana':'ipsa',32:'jyoti',33:'kamal',34:'karan',35:'keshav'}
+print(d)
+d[35]='keshav'
+print
